99bd432b357f6154a9d1650e968795e189d074a8
MuhammadarifS96 <73092935+MuhammadarifS96@users.noreply.github.com>
2023-07-11 23:35:41 -0700

    Waypoint Script

---
+++ BlindGame/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public List<GameObject> Waypoints;
+    public float speed;
+    public bool isLoop = true;
+
+    int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        Vector3 destination = Waypoints[index].transform.position;
+        Vector3 newPos = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+
+        transform.position = newPos;
+
+        float distance = Vector3.Distance(transform.position, destination);
+        if(distance <= 0.05)
+        {
+            if (index < Waypoints.Count - 1)
+            {
+                index++;
+            }
+            else
+            {
+                if (isLoop)
+                {
+                    index = 0;
+                }
+            }
+          
+        }
+
+
+
+    }
+}
